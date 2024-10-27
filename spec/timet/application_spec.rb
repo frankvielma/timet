c19c96d5772b621896545ae46c5a1d5ce1931a86
Frank Vielma <frankvielma@gmail.com>
2024-10-27 09:47:13 -0400

    update spec on application and database

--- spec/timet/application_spec.rb
+++ spec/timet/application_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe Timet::Application do
 
       it 'inserts a new item into the database' do
         app.start('tag', 'my notes...', 25)
-        expect(db).to have_received(:insert_item).with(Time.now.utc.to_i, 'tag', 'my notes...')
+        expect(db).to have_received(:insert_item).with(Time.now.utc.to_i, 'tag', 'my notes...', 25)
       end
 
       it 'calls summary after inserting the item' do
@@ -74,7 +74,7 @@ RSpec.describe Timet::Application do
       it 'inserts a new item into the database with the provided notes from options' do
         app.start('tag')
 
-        expect(db).to have_received(:insert_item).with(1_700_000_000, 'tag', 'my notes from option')
+        expect(db).to have_received(:insert_item).with(1_700_000_000, 'tag', 'my notes from option', 25)
       end
 
       it 'calls play_sound_and_notify with the provided pomodoro time' do
