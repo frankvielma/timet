a9523d66831950a255159095cc884c8c695bc819
Frank Vielma <frankvielma@gmail.com>
2024-12-04 11:33:28 -0400

    refactor: Improve database synchronization logic
    - Refactored `DatabaseSyncHelper` to extract reusable methods for converting items to a hash and processing existing items.
    - Simplified the `sync_databases` method by delegating item processing to a new `process_database_items` method.
    - Enhanced code readability and maintainability by breaking down the synchronization logic into smaller, more focused methods.

--- lib/timet/database_sync_helper.rb
+++ lib/timet/database_sync_helper.rb
@@ -63,6 +63,57 @@ module Timet
       end
     end
 
+    # Converts database items to a hash indexed by ID
+    # @param items [Array<Hash>] Array of database items
+    # @return [Hash] Items indexed by ID
+    def self.items_to_hash(items)
+      items.to_h { |item| [item['id'], item] }
+    end
+
+    # Processes an item that exists in both databases
+    # @param id [Integer] Item ID
+    # @param local_item [Hash] Local database item
+    # @param remote_item [Hash] Remote database item
+    # @param local_db [SQLite3::Database] Local database connection
+    def self.process_existing_item(id, local_item, remote_item, local_db)
+      local_time = local_item['updated_at'].to_i
+      remote_time = remote_item['updated_at'].to_i
+
+      if local_time > remote_time
+        puts "Local item #{id} is newer - will be uploaded"
+      elsif local_time < remote_time
+        puts "Remote item #{id} is newer - updating local"
+        update_item_from_hash(local_db, remote_item)
+      end
+    end
+
+    # Processes items from both databases and syncs them
+    # @param local_db [SQLite3::Database] The local database connection
+    # @param remote_db [SQLite3::Database] The remote database connection
+    # @return [void]
+    def self.process_database_items(local_db, remote_db)
+      remote_items = remote_db.execute('SELECT * FROM items ORDER BY updated_at DESC')
+      local_items = local_db.execute_sql('SELECT * FROM items ORDER BY updated_at DESC')
+
+      remote_by_id = items_to_hash(remote_items)
+      local_by_id = items_to_hash(local_items)
+      all_ids = (remote_by_id.keys + local_by_id.keys).uniq
+
+      all_ids.each do |id|
+        remote_item = remote_by_id[id]
+        local_item = local_by_id[id]
+
+        if remote_item && local_item
+          process_existing_item(id, local_item, remote_item, local_db)
+        elsif remote_item
+          puts "Adding remote item #{id} to local"
+          insert_item_from_hash(local_db, remote_item)
+        else # local_item exists
+          puts "Local item #{id} will be uploaded"
+        end
+      end
+    end
+
     # Synchronizes the local and remote databases by comparing and merging their items
     #
     # @param local_db [SQLite3::Database] The local database connection
@@ -72,41 +123,7 @@ module Timet
     # @param local_db_path [String] Path to the local database file
     # @return [void]
     def self.sync_databases(local_db, remote_db, s3, bucket, local_db_path)
-      remote_items = remote_db.execute('SELECT * FROM items ORDER BY updated_at DESC')
-      local_items = local_db.execute_sql('SELECT * FROM items ORDER BY updated_at DESC')
-
-      # Convert items to hash for easier comparison
-      remote_by_id = remote_items.to_h { |item| [item['id'], item] }
-      local_by_id = local_items.to_h { |item| [item['id'], item] }
-
-      # Find items that need syncing
-      all_ids = (remote_by_id.keys + local_by_id.keys).uniq
-
-      all_ids.each do |id|
-        remote_item = remote_by_id[id]
-        local_item = local_by_id[id]
-
-        if remote_item && local_item
-          local_time = local_item['updated_at'].to_i
-          remote_time = remote_item['updated_at'].to_i
-
-          if local_time > remote_time
-            puts "Local item #{id} is newer - will be uploaded"
-          elsif local_time < remote_time
-            puts "Remote item #{id} is newer - updating local"
-            update_item_from_hash(local_db, remote_item)
-          else
-            puts "Item #{id} has same timestamp - no update needed"
-          end
-        elsif remote_item
-          puts "Adding remote item #{id} to local"
-          insert_item_from_hash(local_db, remote_item)
-        elsif local_item
-          puts "Local item #{id} will be uploaded"
-        end
-      end
-
-      # Upload the merged database back to S3
+      process_database_items(local_db, remote_db)
       s3.upload_file(bucket, local_db_path, 'timet.db')
       puts 'Database sync completed'
     end
