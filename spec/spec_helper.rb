d34cda828c3fda967c49cd8592901e0dac07ff52
Frank Vielma <frankvielma@gmail.com>
2025-02-11 12:26:12 -0400

    update spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,6 @@ require 'simplecov-lcov'
 
 SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true  # Ensure a single lcov.info file
 SimpleCov::Formatter::LcovFormatter.config.output_directory = 'coverage'   # Save it in coverage/
-SimpleCov::Formatter::LcovFormatter.config.output_name = 'lcov.info'       # Ensure correct filename
 
 SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 SimpleCov.start
